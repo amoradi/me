adec86bb33c6e11a3fa3a2d91c460e7cc5fa9c83
amoradi <aaron.moradi@daveramsey.com>
2017-09-30 18:37:12 -0500

    unprotect subjects route

--- server/api/subject/subjectRoutes.js
+++ server/api/subject/subjectRoutes.js
@@ -4,10 +4,10 @@ var auth = require('../../auth/auth');
 var checkUser = [auth.decodeToken(), auth.getFreshUser()];
 
 router.param('id', controller.params);
-
+// checkUser,
 router.route('/')
   .get(controller.get)
-  .post(checkUser, controller.post)
+  .post(controller.post)
 
 router.route('/:id')
   .get(controller.getOne)

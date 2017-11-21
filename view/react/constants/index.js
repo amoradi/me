0ac6400096206d0bdd1c1ff1dff0189fafe6826f
Aaron Moradi <moradi.aaron@gmail.com>
2017-11-20 20:57:04 -0600

    add std icon constants

--- view/react/constants/index.js
+++ view/react/constants/index.js
@@ -1,8 +1,8 @@
 export const LS_NOTES_TOKEN = 'amoradi-notes-token';
 export const IMG_ME_AND_FAM = '/assets/images/me_and_fam.jpg';
-export const IMG_NOTES = '/assets/images/notes-icon-gray.svg';
-export const IMG_ARTIFACTS = '/assets/images/artifacts-icon.svg';
-export const IMG_INFO = '/assets/images/info-icon.svg';
+export const IMG_NOTES = '/assets/images/notes-icon-std.svg';
+export const IMG_ARTIFACTS = '/assets/images/artifacts-icon-std.svg';
+export const IMG_INFO = '/assets/images/info-icon-std.svg';
 
 /* routes */
 export const HOME = '/';
